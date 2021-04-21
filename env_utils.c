43b647059387637a21d813f05cdc822c28c76727
Maxime TOGBE <mtogbe@e2r2p18.42.fr>
2021-04-21 18:06:19 +0200

    fix

--- env_utils.c
+++ env_utils.c
@@ -6,7 +6,7 @@
 /*   By: mtogbe <mtogbe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/21 16:58:50 by mtogbe            #+#    #+#             */
-/*   Updated: 2021/04/21 17:20:03 by mtogbe           ###   ########.fr       */
+/*   Updated: 2021/04/21 18:04:43 by mtogbe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 void	sort_env(t_env **env)
 {
 	t_env	*head;
-	char	*tmp;
+	char	*tmp_key;
+	char	*tmp_value;
 	int		reset;
 
 	reset = 0;
@@ -25,9 +26,12 @@ void	sort_env(t_env **env)
 		reset = 0;
 		if (ft_strcmp((*env)->key, (*env)->next->key) > 0)
 		{
-			tmp = (*env)->key;
+			tmp_key = (*env)->key;
+			tmp_value = (*env)->value;
 			(*env)->key = (*env)->next->key;
-			(*env)->next->key = tmp;
+			(*env)->value = (*env)->next->value;
+			(*env)->next->key = tmp_key;
+			(*env)->next->value = tmp_value;
 			reset = 1;
 		}
 		*env = (*env)->next;
