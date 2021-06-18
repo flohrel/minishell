5842b613ddd1cc9fcf02f8a226d398d5754d3e24
Maxime TOGBE <mtogbe@e3r1p2.42.fr>
2021-06-18 17:11:22 +0200

    free error

--- src/builtins/assign.c
+++ src/builtins/assign.c
@@ -6,7 +6,7 @@
 /*   By: mtogbe <mtogbe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 17:02:16 by mtogbe            #+#    #+#             */
-/*   Updated: 2021/06/18 15:48:51 by mtogbe           ###   ########.fr       */
+/*   Updated: 2021/06/18 17:11:08 by mtogbe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,21 @@ int	manage_agn(t_list *assign, t_vars *vars, t_env *block)
 	if (ft_endwith(block->key, '+'))
 	{
 		write(1, "de\n", 3);
+		(block->key)[ft_strlen(block->key) - 2] = '\0';
+		//chercher egalement la valeur dans la list export
 		stack = get_env_value(block->key, vars->agn);
+		if (!stack)
+			stack = ft_strdup("");
 		stackb = block->value;
 		block->value = ft_strjoin(stack, block->value);
-		free(stack);
-		free(stackb);
+//		pourauoi ca free un truc pas alloue
+//		free(stack);
+//		free(stackb);
 	}
 	if (!vars->agn)
-		vars->agn = block;
+		vars->agn = blockcpy(block);
 	else
 		add_to_exp(vars->agn, block);
-	env_print(vars->agn);
 	return (1);
 }
 
@@ -39,6 +43,7 @@ int	handle_assign(t_vars *vars, t_list *assign)
 {
 	t_env	*res;
 
+	env_print(vars->agn);
 	while (assign)
 	{
 		res = malloc(sizeof(t_env));
@@ -46,7 +51,6 @@ int	handle_assign(t_vars *vars, t_list *assign)
 			return (0);
 		if (new_envblock(((t_token *)(assign->content))->data, res) == 0)
 			return (0);
-		exp_print(res);
 		manage_agn(assign, vars, res);
 		free(res);
 		assign = assign->next;
