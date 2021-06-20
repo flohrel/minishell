656f81fd84a74e1e9b19d4fb9142b8de17d98a34
Maxime TOGBE <mtogbe@e3r7p20.42.fr>
2021-06-20 16:50:29 +0200

    oui

--- src/builtins/assign.c
+++ src/builtins/assign.c
@@ -6,41 +6,49 @@
 /*   By: mtogbe <mtogbe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 17:02:16 by mtogbe            #+#    #+#             */
-/*   Updated: 2021/06/18 17:11:08 by mtogbe           ###   ########.fr       */
+/*   Updated: 2021/06/20 16:49:47 by mtogbe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-int	manage_agn(t_list *assign, t_vars *vars, t_env *block)
+int	assign_add(t_env *block, t_vars *vars)
 {
 	char	*stack;
 	char	*stackb;
-	
+
+	(block->key)[ft_strlen(block->key) - 2] = '\0';
+	//chercher egalement la valeur dans la list export
+	stack = get_env_value(block->key, vars->exp);
+	if (!stack)
+		stack = get_env_value(block->key, vars->agn);
+	if (!stack)
+		stack = ft_strdup("\0");
+	stackb = block->value;
+	block->value = ft_strjoin(stack, block->value);
+	if (!(block->value))
+			return (-1);
+	free(stackb);
+	return (1);
+}
+
+int	manage_agn(t_list *assign, t_vars *vars, t_env *block)
+{
 	(void)assign;
 	if (ft_endwith(block->key, '+'))
-	{
-		write(1, "add\n", 4);
-		(block->key)[ft_strlen(block->key) - 2] = '\0';
-		//chercher egalement la valeur dans la list export
-		stack = get_env_value(block->key, vars->env);
-		if (!stack)
-			stack = get_env_value(block->key, vars->agn);
-		if (!stack)
-			stack = ft_strdup("\0");
-		stackb = block->value;
-		block->value = ft_strjoin(stack, block->value);
-		if (!(block->value))
-				return (-1);
-		free(stackb);
-	}
+		if (assign_add(block, vars) < 0)
+			return (-1);
+	if (get_env_value(block->key, vars->env))
+		if (add_to_exp(vars->env, block) < 0)
+			return (-1);
+	if (get_env_value(block->key, vars->exp))
+		if (add_to_exp(vars->exp, block) < 0)
+			return (-1);
 	if (!vars->agn)
 		vars->agn = blockcpy(block);
 	else
 		if (add_to_exp(vars->agn, block) < 0)
 			return (-1);
-	env_print(vars->agn);
-	env_print(block);
 	return (1);
 }
 
--- src/exec/exec.c
+++ src/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: mtogbe <mtogbe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 19:05:43 by mtogbe            #+#    #+#             */
-/*   Updated: 2021/06/18 15:50:13 by mtogbe           ###   ########.fr       */
+/*   Updated: 2021/06/20 16:41:43 by mtogbe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ int	exec_command(t_vars *vars, t_ast *node)
 	
 	param = node->data;
 	args = list_to_tab(param->arg, vars);
-	handle_assign(vars, param->assign);
+	if (param && !(param->path))
+		handle_assign(vars, param->assign);
 	if (find_builtin(param->path, args, vars))
 		return (2);
 	else if (find_cmd(param->path, args,
