427140007bdf872cb0dfb91398b73cb7eeae2401
flohrel <flohrel@student.42.fr>
2021-06-29 16:22:26 +0200

    redir

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -6,40 +6,11 @@
 /*   By: mtogbe <mtogbe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 19:05:43 by mtogbe            #+#    #+#             */
-/*   Updated: 2021/06/29 01:03:02 by flohrel          ###   ########.fr       */
+/*   Updated: 2021/06/29 16:22:18 by flohrel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-void	parse_redir(t_param *param, t_rd *redir)
-{
-	t_list	*lst;
-	t_token	*token;
-
-	redir->field = 0;
-	lst = param->redir;
-	if (lst == NULL)
-		return ;
-	while (lst)
-	{
-		token = (t_token *)lst->content;
-		if (token->type == TK_LESS)
-		{
-			set_flag(&redir->field, RD_IN);
-			redir->fd_in = open(token->data, O_RDONLY);
-		}
-		lst = lst->next;
-	}
-}
-
-void	init_cmd(t_vars *vars, t_param *param)
-{
-	t_cmd	*cmd;
-
-	cmd = lst_alloc(1, sizeof(*cmd), vars);
-	parse_redir(param, &cmd.redir);
-}
 
 int	exec_command(t_vars *vars, t_ast *node)
 {
--- src/exec/redirections.c
+++ src/exec/redirections.c
@@ -1,15 +1,47 @@
 #include <minishell.h>
 
-int	handle_redirections(t_param *param)
+void	set_rdout(t_cmd *cmd)
 {
-	t_token	*tk_redir;
+	set_flag(&cmd->io_bit, RD_OUT);
+	clear_flag(&cmd->io_bit, RD_APP);
+	cmd->redir[FD_OUT] = open(
+}
 
-	while (param->redir)
+void	set_rdapp(t_cmd *cmd)
+{
+	set_flag(&cmd->io_bit, RD_OUT | RD_APP);
+}
+
+void	set_rdin(t_cmd *cmd)
+{
+	set_flag(&cmd->io_bit, RD_IN);
+	clear_flag(&cmd->io_bit, RD_HDOC);
+}
+
+void	set_hdoc(t_cmd *cmd)
+{
+	set_flag(&cmd->io_bit, RD_IN | RD_HDOC);
+}
+
+void	parse_redir(t_vars *vars, t_param *param)
+{
+	t_list	*lst;
+	t_token	*token;
+
+	lst = param->redir;
+	if (lst == NULL)
+		return ;
+	while (lst)
 	{
-		tk_redir = (t_token *)(param->redir->content);
-		if (tk_redir->type == TK_GREAT)
-			write(1, "o", 1);
-		param->redir = param->redir->next;
+		token = (t_token *)lst->content;
+		if (token->type == TK_LESS)
+			set_rdout(&vars->cmd, token->data);
+		else if (token->type == TK_DLESS)
+			set_rdapp(&vars->cmd, token->data);
+		else if (token->type == TK_GREAT)
+			set_rdin(&vars->cmd, token->data);
+		else
+			set_hdoc(&vars->cmd, token->data);
+		lst = lst->next;
 	}
-	return (1);
 }
