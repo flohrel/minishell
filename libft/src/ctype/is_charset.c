b92ca6a88d9ea5a69eb3632fdf2f9c47ee5e2559
flohrel <flohrel@student.42.fr>
2021-08-06 15:55:38 +0200

    libft security

--- libft/src/ctype/is_charset.c
+++ libft/src/ctype/is_charset.c
@@ -6,7 +6,7 @@
 /*   By: flohrel <flohrel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/03 17:51:46 by flohrel           #+#    #+#             */
-/*   Updated: 2021/06/03 17:56:10 by flohrel          ###   ########.fr       */
+/*   Updated: 2021/08/05 19:53:42 by flohrel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,14 @@
 
 int	is_charset(const char *set, char c)
 {
-	while (*set)
+	if (set)
 	{
-		if (c == *set)
-			return (1);
-		set++;
+		while (*set)
+		{
+			if (c == *set)
+				return (1);
+			set++;
+		}
 	}
 	return (0);
 }
