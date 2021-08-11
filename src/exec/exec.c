d4a7d78ef039e70e85c902d53fce847ff89b5946
flohrel <flohrel@student.42.fr>
2021-08-11 20:34:03 +0200

    doesn't work

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -6,28 +6,24 @@
 /*   By: mtogbe <mtogbe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 19:05:43 by mtogbe            #+#    #+#             */
-/*   Updated: 2021/08/08 17:22:11 by flohrel          ###   ########.fr       */
+/*   Updated: 2021/08/11 20:29:48 by flohrel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
 
-int	exec_command(t_vars *vars, t_cmd *cmd, t_ast *node)
+void	exec_command(t_vars *vars, t_ast *node)
 {
 	t_param	*param;
 	char	**args;
 
-	(void)cmd;
 	if (node == NULL)
-		return (-1);
+		return ;
 	param = node->data;
 	args = list_to_tab(param->arg, vars);
 	if (param && !(param->path))
 		handle_assign(vars, param->assign);
-	if (find_cmd(param, args,
-			env_to_tab(vars->env, vars), vars))
-		return (3);
-	return (-1);
+	find_cmd(param, args, env_to_tab(vars->env, vars), vars);
 }
 
 void	exec_pipeline(t_vars *vars, t_cmd *cmd, t_ast *node)
@@ -37,22 +33,22 @@ void	exec_pipeline(t_vars *vars, t_cmd *cmd, t_ast *node)
 	cmd->std_out = dup(FD_OUT);
 	cmd->std_in = dup(FD_IN);
 	pipe(fdes);
-	cmd->pipe[FD_OUT] = fdes[1];
-	cmd->pipe[FD_IN] = fdes[0];
+	cmd->pipe[FD_OUT] = fdes[FD_OUT];
+	cmd->pipe[FD_IN] = fdes[FD_IN];
 	set_flag(&cmd->io_bit, PIPE_IN);
-	exec_command(vars, cmd, node->left);
+	exec_command(vars, node->left);
 	node = node->right;
 	while (node && (node->type == NODE_PIPE))
 	{
 		set_flag(&cmd->io_bit, PIPE_OUT);
 		pipe(fdes);
-		cmd->pipe[FD_OUT] = fdes[1];
-		exec_command(vars, cmd, node->left);
-		cmd->pipe[FD_IN] = fdes[0];
+		cmd->pipe[FD_OUT] = fdes[FD_OUT];
+		exec_command(vars, node->left);
+		cmd->pipe[FD_IN] = fdes[FD_IN];
 		node = node->right;
 	}
 	cmd->io_bit = -1;
-	exec_command(vars, cmd, node);
+	exec_command(vars, node);
 }
 
 void	exec_job(t_vars *vars, t_ast *node)
@@ -60,7 +56,7 @@ void	exec_job(t_vars *vars, t_ast *node)
 	if (node->type == NODE_PIPE)
 		exec_pipeline(vars, &vars->cmd, node);
 	else
-		exec_command(vars, &vars->cmd, node);
+		exec_command(vars, node);
 }
 
 void	exec_list(t_vars *vars, t_ast *node, bool is_exec)
@@ -68,9 +64,9 @@ void	exec_list(t_vars *vars, t_ast *node, bool is_exec)
 	vars->cmd.io_bit = 0;
 	vars->cmd.std_out = -1;
 	vars->cmd.std_in = -1;
-	if (node && !check_flag(node->type, NODE_LIST) && (is_exec == true))
+	if (!check_flag(node->type, NODE_LIST))
 		exec_job(vars, node);
-	else if (node)
+	else
 	{
 		if (is_exec == true)
 			exec_job(vars, node->left);
@@ -79,7 +75,12 @@ void	exec_list(t_vars *vars, t_ast *node, bool is_exec)
 		else if ((node->type == NODE_OR) && (exit_status))
 			exec_list(vars, node->right, true);
 		else
-			exec_list(vars, node->right, false);
+		{
+			if (is_exec == false)
+				exec_list(vars, node->right, true);
+			else
+				exec_list(vars, node->right, false);
+		}
 	}
 }
 
