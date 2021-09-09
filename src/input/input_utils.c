4882121e4ca5297fab3528e55ee900af9e6f3151
flohrel <flohrel@student.42.fr>
2021-09-09 17:49:27 +0200

    input still need to be fixed

--- src/input/input_utils.c
+++ src/input/input_utils.c
@@ -6,7 +6,7 @@
 /*   By: flohrel <flohrel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/08 20:32:35 by flohrel           #+#    #+#             */
-/*   Updated: 2021/09/09 17:32:21 by flohrel          ###   ########.fr       */
+/*   Updated: 2021/09/09 17:49:01 by flohrel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 char	*display_prompt(void)
 {
-	if (isatty(0))
+	if (isatty(0) && (g_sig.exit_status != 130))
 	{
 		if (!g_sig.exit_status)
 			return (readline(PROMPT1));
